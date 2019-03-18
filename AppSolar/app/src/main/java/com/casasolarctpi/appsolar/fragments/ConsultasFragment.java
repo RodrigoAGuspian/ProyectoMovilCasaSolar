7bd9d777914e670411606c93cf303ea9211ec63d
RodrigoAGuspian <raguspian@misena.edu.co>
2019-03-18 17:49:23 -0500

    arreglar el codigo del chartDay

--- AppSolar/app/src/main/java/com/casasolarctpi/appsolar/fragments/ConsultasFragment.java
+++ AppSolar/app/src/main/java/com/casasolarctpi/appsolar/fragments/ConsultasFragment.java
@@ -5,9 +5,11 @@ import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
+import android.app.DownloadManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,13 +25,26 @@ import android.widget.Toast;
 import com.casasolarctpi.appsolar.R;
 import com.casasolarctpi.appsolar.controllers.MenuPrincipal;
 import com.casasolarctpi.appsolar.models.Constants;
+import com.casasolarctpi.appsolar.models.DatosTH;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,15 +53,21 @@ public class ConsultasFragment extends Fragment implements OnClickListener, OnDa
     View view;
     TabHost tabHost;
     Button btnConsulta1, btnConsulta2, btnConsulta3;
-    BarChart barChart1, barChart2, barChart3;
+    BarChart barChart1, barChart2;
+    LineChart lineChart1;
     EditText txtDate1, txtDate2;
     MaterialSpinner mSMes;
     NumberPicker nPAnio;
     Date dateToQuery;
-    Date dateDay;
-
+    String fechaATexto;
     Dialog dialog;
     int mode;
+    List<DatosTH> datosGenerales = new ArrayList<>();
+    private List<ILineDataSet> dataSets = new ArrayList<>();
+    List<DatosTH> datosTHList = new ArrayList<>();
+    public static List<String> labelsChart = new ArrayList<>();
+
+
     public ConsultasFragment() {
         // Required empty public constructor
     }
@@ -59,6 +80,7 @@ public class ConsultasFragment extends Fragment implements OnClickListener, OnDa
         view = inflater.inflate(R.layout.fragment_consultas, container, false);
         iniziliate();
         inputDataToSpinner();
+        inputDataOfFirebase();
         return view;
     }
 
@@ -88,9 +110,13 @@ public class ConsultasFragment extends Fragment implements OnClickListener, OnDa
         btnConsulta2 = view.findViewById(R.id.btnConsulta2);
         btnConsulta3 = view.findViewById(R.id.btnConsulta3);
 
+        btnConsulta1.setOnClickListener(this);
+        btnConsulta2.setOnClickListener(this);
+        btnConsulta3.setOnClickListener(this);
+
+        lineChart1 = view.findViewById(R.id.lineChart1);
         barChart1 = view.findViewById(R.id.barChart1);
         barChart2 = view.findViewById(R.id.barChart2);
-        barChart3 = view.findViewById(R.id.barChart3);
 
         txtDate1 = view.findViewById(R.id.txtConsulta1);
         txtDate2 = view.findViewById(R.id.txtConsulta2);
@@ -112,6 +138,26 @@ public class ConsultasFragment extends Fragment implements OnClickListener, OnDa
         nPAnio.setMaxValue(Constants.MAX_YEAR);
         nPAnio.setValue(year);
 
+
+
+    }
+
+    private void inputDataOfFirebase() {
+        DatabaseReference datos = MenuPrincipal.reference.child("datos");
+        datos.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<DatosTH>> t = new GenericTypeIndicator<List<DatosTH>>() {};
+                datosGenerales = dataSnapshot.getValue(t);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
@@ -212,17 +258,53 @@ public class ConsultasFragment extends Fragment implements OnClickListener, OnDa
         switch (mode) {
             case 1:
                 int realMonth = month + 1;
-                String date = dayOfMonth + "-" + realMonth + "-" + year;
+                fechaATexto = dayOfMonth + "-" + realMonth + "-" + year;
                 Calendar calendar = new GregorianCalendar(year,month,dayOfMonth);
                 dateToQuery = calendar.getTime();
-                txtDate1.setText(date);
-                dateDay = new GregorianCalendar(year,month,dayOfMonth).getTime();
+                txtDate1.setText(fechaATexto);
+                //dateDay = new GregorianCalendar(year,month,dayOfMonth).getTime();
                 showChartDay();
                 break;
         }
     }
 
     private void showChartDay() {
+        List<Entry> entries = new ArrayList<>();
+        List<Entry> entries1 = new ArrayList<>();
+        List<DatosTH> datosFiltrado = new ArrayList<>();
+
+
+
+        for (int i=0;i<datosGenerales.size();i++){
+            DatosTH datosTH = datosGenerales.get(i);
+            if (datosTH.getFecha_dato().equals(fechaATexto)){
+                try {
+                    float dato1 = Float.parseFloat(datosTH.getHumedad());
+                    float dato2 = Float.parseFloat(datosTH.getTemperatura());
+
+                }catch (Exception ignored){
+
+                }
+            }
+
+            //Organizar codigo
+            labelsChart.add(datosTHList.get(i).getHora());
+            float dato1=0;
+            float dato2=0;
+            try {
+                dato1=Float.parseFloat(datosTHList.get(i).getHumedad());
+                dato2=Float.parseFloat(datosTHList.get(i).getTemperatura());
+
+            }catch (Exception ignore){
+
+            }
+            entries.add(new Entry(i, dato1));
+            entries1.add(new Entry(i, dato2));
+
+
+        }
+
+
 
 
     }
